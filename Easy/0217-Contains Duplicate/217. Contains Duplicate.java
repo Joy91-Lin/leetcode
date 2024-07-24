4908b8531c2bec7fa60122ff7e051eb301edac3f
Joy <joy.chiaying@gmail.com>
2024-07-25 00:27:23 +0800

    217. Contains Duplicate

---
+++ Easy/0217-Contains Duplicate/217. Contains Duplicate.java
@@ -0,0 +1,18 @@
+/**
+ * 217. Contains Duplicate
+ * 
+ * T: O(n)
+ * S: O(n)
+ **/
+
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0; i < nums.length; i++){
+            if (!set.add(nums[i]))  
+                return true;
+        }
+        return false;
+    }
+}
